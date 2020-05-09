970a6e705dfb65a2d1ed7489b17f7bae97fcef07
Constantinescu <gabrielovici123@gmail.com>
2020-05-09 11:34:11 +0300

    Initializare componente

---
+++ Proiect_SWA_CG_ARDUINO/Proiect_SWA_CG_ARDUINO.ino
@@ -0,0 +1,30 @@
+#include <Wire.h> 
+#include <Servo.h>
+
+
+#define a1 A0 //Motor A1   F
+#define a2 A1 //Motor A2   R
+#define b1 A2 //Motor B1   F
+#define b2 A3 //Motor B2   R
+
+int trig = 8;
+int echo = 7;
+
+
+int s1 =    digitalRead(2);     //senzor 1
+int s2 =    digitalRead(3);     //senzor 2
+int s3 =    digitalRead(4);     //senzor 3
+int s4 =    digitalRead(5);     //senzor 4
+int s5 =    digitalRead(6);     //senzor 5
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
